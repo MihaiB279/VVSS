acd647fd64ff5228ab3c804501664a81b5e5b9d1
Bucur Mihai - Alexandru <mihai.alexandru.bucur@stud.ubbcluj.ro>
2024-04-12 00:34:32 +0300

    Update with tests

--- Mosch/src/test/java/tasks/model/TasksOperationsTest.java
+++ Mosch/src/test/java/tasks/model/TasksOperationsTest.java
@@ -35,6 +35,7 @@ class TasksOperationsTest {
 
         // Assert
         Assertions.assertEquals(result.size(), 1);
+        Assertions.assertEquals(result.get(0).getTitle(), new Task("Empty", new Date()).getTitle());
     }
 
     @Test
@@ -77,6 +78,7 @@ class TasksOperationsTest {
 
         // Assert
         Assertions.assertEquals(1, result.size());
+        Assertions.assertEquals(result.get(0).getTitle(), new Task("Empty", new Date()).getTitle());
     }
 
     @Test
@@ -98,8 +100,85 @@ class TasksOperationsTest {
 
         // Assert
         Assertions.assertEquals(result.size(), 1);
+        Assertions.assertEquals(result.get(0).getTitle(), new Task("Empty", new Date()).getTitle());
     }
 
 
+    @Test
+    void F02_incoming_StartlIsNull_returnOne() {
+        // Arrange
+        ObservableList<Task> tasks = FXCollections.observableArrayList();
 
+        tasks.add(new Task("TASK1", new Date(80, 04, 15, 12,30)));
+        // valid
+        tasks.add(new Task("TASK2", new Date(80, 04, 20, 12,30)));
+        tasks.add(new Task("TASK3", new Date(80, 04, 25, 12,30)));
+        // end valid
+        tasks.add(new Task("TASK4", new Date(80, 04, 30, 12,30)));
+
+        TasksOperations top = new TasksOperations(tasks);
+
+        // Act
+        ArrayList<Task> result = (ArrayList<Task>) top.incoming(null, new Date(80, 04, 22, 12,30));
+
+        // Assert
+        Assertions.assertEquals(result.size(), 1);
+        Assertions.assertEquals(result.get(0).getTitle(), new Task("Empty", new Date()).getTitle());
+    }
+
+    @Test
+    void F02_incoming_EndIsNull_returnOne() {
+        // Arrange
+        ObservableList<Task> tasks = FXCollections.observableArrayList();
+
+        tasks.add(new Task("TASK1", new Date(80, 04, 15, 12,30)));
+        // valid
+        tasks.add(new Task("TASK2", new Date(80, 04, 20, 12,30)));
+        tasks.add(new Task("TASK3", new Date(80, 04, 25, 12,30)));
+        // end valid
+        tasks.add(new Task("TASK4", new Date(80, 04, 30, 12,30)));
+
+        TasksOperations top = new TasksOperations(tasks);
+
+        // Act
+        ArrayList<Task> result = (ArrayList<Task>) top.incoming(new Date(80, 04, 21, 12,30), null);
+
+        // Assert
+        Assertions.assertEquals(result.size(), 1);
+        Assertions.assertEquals(result.get(0).getTitle(), new Task("Empty", new Date()).getTitle());
+    }
+
+    @Test
+    void F02_incoming_tasksHasOneElementAndHasDatesInInterval_returnExpectedTasks() {
+        // Arrange
+        ObservableList<Task> tasks = FXCollections.observableArrayList();
+
+        tasks.add(new Task("TASK1", new Date(80, 04, 22, 12,30)));
+
+        TasksOperations top = new TasksOperations(tasks);
+
+        // Act
+        ArrayList<Task> result = (ArrayList<Task>) top.incoming(new Date(80, 04, 17, 12,30), new Date(80, 04, 26, 12,30));
+
+        // Assert
+        Assertions.assertEquals(1, result.size());
+    }
+
+    @Test
+    void F02_incoming_tasksHasTwoElementsAndHasDatesInInterval_returnExpectedTasks() {
+        // Arrange
+        ObservableList<Task> tasks = FXCollections.observableArrayList();
+
+        tasks.add(new Task("TASK1", new Date(80, 04, 20, 12,30)));
+        // valid
+        tasks.add(new Task("TASK2", new Date(80, 04, 21, 12,30)));
+
+        TasksOperations top = new TasksOperations(tasks);
+
+        // Act
+        ArrayList<Task> result = (ArrayList<Task>) top.incoming(new Date(80, 04, 17, 12,30), new Date(80, 04, 26, 12,30));
+
+        // Assert
+        Assertions.assertEquals(2, result.size());
+    }
 }
